373386fcbca47c28802b2db313bdd0d23d03364b
Jonathan Beisiegel <jonathan.beisiegel@netpioneer.de>
2016-04-06 09:35:18 +0200

    changed user to static

--- src/main/java/de/dhbw/ds/cdn/controller/UserController.java
+++ src/main/java/de/dhbw/ds/cdn/controller/UserController.java
@@ -2,15 +2,22 @@ package de.dhbw.ds.cdn.controller;
 
 import de.dhbw.ds.cdn.data.User;
 import de.dhbw.ds.cdn.repositries.UserRepository;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
 
 @RestController
 public class UserController {
 
 	private final UserRepository userRepository;
 
-	@Autowired
+    @Autowired
 	public UserController(UserRepository userRepository) {
 		this.userRepository = userRepository;
 	}
@@ -20,11 +27,31 @@ public class UserController {
 		return userRepository.findAll();
 	}
 
+	@RequestMapping("/user")
+	@ResponseBody
+	public User getUserById(UUID id){
+		User user = userRepository.findById(id);
+		return user;
+	}
+
 	@RequestMapping(method = RequestMethod.POST, path = "/users")
     @ResponseBody
 	public User setUser(@RequestBody User user){
+		user.setId(randomUUID());
         userRepository.save(user);
         return user;
 	}
+
+    @RequestMapping(method = RequestMethod.POST, path = "/login")
+    @ResponseBody
+    @ExceptionHandler
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public User getUserById(@RequestBody User user) throws Exception {
+        User userDB = userRepository.findByLogin(user.getLogin());
+        if(!StringUtils.equals(userDB.getPassword(),user.getPassword())){
+            throw new Exception("User not found");
+        }
+        return userDB;
+    }
 
 }
--- src/main/java/de/dhbw/ds/cdn/repositries/UserRepository.java
+++ src/main/java/de/dhbw/ds/cdn/repositries/UserRepository.java
@@ -3,8 +3,12 @@ package de.dhbw.ds.cdn.repositries;
 import de.dhbw.ds.cdn.data.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.UUID;
+
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
     User findByLogin(String login);
+
+    User findById(UUID id);
 }
