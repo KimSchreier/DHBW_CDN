858a7f4c8f8d5adab6422445eb94d2b865d63bbc
Jonathan Beisiegel <jonathan.beisiegel@netpioneer.de>
2016-04-13 11:36:12 +0200

    changed pom and application to deploy as war

--- src/main/java/de/dhbw/ds/cdn/ShopApplication.java
+++ src/main/java/de/dhbw/ds/cdn/ShopApplication.java
@@ -2,9 +2,16 @@ package de.dhbw.ds.cdn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ShopApplication {
+public class ShopApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ShopApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
